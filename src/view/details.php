367389c9b9b466b429de4fc66ed045de3a9bd658
vuongducbao <94464272+vuongducbao@users.noreply.github.com>
2022-01-06 15:22:16 +0700

    Create details.php

---
+++ src/view/details.php
@@ -0,0 +1,23 @@
+<?php include("template/header.php");
+    echo $data['magv'];
+    echo('<br>');
+    echo $data['hovaten'];
+    echo('<br>');
+    echo $data['ngaysinh'];
+    echo('<br>');
+    echo $data['gioitinh'];
+    echo('<br>');
+    echo $data['trinhdo'];
+    echo('<br>');
+    echo $data['chuyenmon'];
+    echo('<br>');
+    echo $data['hocham'];
+    echo('<br>');
+    echo $data['hocvi'];
+    echo('<br>');
+    echo $data['coquan'];
+    echo('<br>');
+    ?>
+    <a href="index.php">Back</a>
+    <?php
+include("template/footer.php"); ?>
