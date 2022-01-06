eddebe482ced581f9b7bd90475842a2d3563a063
vuongducbao <94464272+vuongducbao@users.noreply.github.com>
2022-01-06 15:22:04 +0700

    Create add.php

---
+++ src/view/add.php
@@ -0,0 +1,19 @@
+<?php include("template/header.php"); ?>
+    <form method="post">
+        <input type="text" placeholder="Mã giảng viên" name="txtmagv">
+        <br>
+        <input type="text" placeholder="Họ và tên" name="txthovaten">
+        <br>
+        <input type="text" placeholder="Ngày sinh" name="txtngaysinh">
+        <br>
+        <input type="text" placeholder="Giới tính" name="txtgioitinh">
+        <br>
+        <input type="text" placeholder="Trình độ" name="txttrinhdo">
+        <br>
+        <input type="text" placeholder="Chuyên môn" name="txtchuyenmon"><br>
+        <input type="text" placeholder="Học hàm" name="txthocham"><br>
+        <input type="text" placeholder="Hoc vị" name="txthocvi"><br>
+        <input type="text" placeholder="Cơ quan" name="txtcouan"><br>
+        <button type="submit">Lưu</button>
+    </form>
+<?php include("template/footer.php"); ?>
